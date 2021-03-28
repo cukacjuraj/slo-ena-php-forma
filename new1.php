ec624a42a3b34c910094639b390d0356f6553e32
cukacjuraj <73613502+cukacjuraj@users.noreply.github.com>
2021-03-28 16:52:33 +0200

    Add files via upload

---
+++ new1.php
@@ -0,0 +1,231 @@
+<html>
+<head>
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<style> 
+table, th, td {
+  border: 1px solid black;
+}
+
+table {
+  border-collapse: collapse;
+  width: 50%;
+}
+
+th {
+  height: 20px;
+}
+</style>
+</head>
+<body>
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+<br>
+<br><br><br>
+		<p> Button</p>
+        <button 
+          type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon-edit"></span> Login
+        </button>
+		<br>
+		<br>
+		<p> Progress bar</p>
+<div class="container">
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width:45%" >
+      45%
+    </div>
+  </div>
+</div>
+ <p> Grid </p>
+ 
+ <table style="width:100%">
+  <tr>
+    <th>Order ID</th>
+    <th>Customer Name</th> 
+    <th>Freight</th>
+	<th>Date</th>
+  </tr>
+  <tr>
+    <td>123355</td>
+	<td>VINET</td>
+	<td>$3.4</td>
+	<td>3/6/15</td>
+    
+	
+  </tr>
+  <tr>
+    <td>124455</td>
+    <td>WELOI</td>
+	<td>4</td>
+	<td>4/6/15</td>
+    
+	
+  </tr>
+  <tr>
+    <td>124455</td>
+	<td>CHANG</td>
+	<td>3</td>
+    <td>5/6/15</td>
+  </tr>
+  <tr>
+    <td>124455</td>
+	<td>CHANG</td>
+    <td>3</td>
+	<td>8/6/15</td>
+  </tr>
+  
+</table>
+
+<form action="php2.php" method="post">
+E-mail ocjenjivača:<input type="email" name="email"><br><br>
+Datum ocjenjivanja: <input type="date" name="date"><br><br>
+Aktualnost sadržaja:<br> 
+  <input type="radio" id="5" name="gender" value="5">
+  <label for="5">5</label><br>
+  <input type="radio" id="4" name="gender" value="4">
+  <label for="4">4</label><br>
+  <input type="radio" id="3" name="gender" value="3">
+  <label for="3">3</label><br>
+  <input type="radio" id="2" name="gender" value="2">
+  <label for="2">2</label><br>
+  <input type="radio" id="1" name="gender" value="1">
+  <label for="1">1</label><br><br>
+  Pripremljenost predavaca:<br> 
+  <input type="radio" id="5" name="gender" value="5">
+  <label for="5">5</label><br>
+  <input type="radio" id="4" name="gender1" value="4">
+  <label for="4">4</label><br>
+  <input type="radio" id="3" name="gender1" value="3">
+  <label for="3">3</label><br>
+  <input type="radio" id="2" name="gender1" value="2">
+  <label for="2">2</label><br>
+  <input type="radio" id="1" name="gender1" value="1">
+  <label for="1">1</label><br><br>
+  Zanimljivost teme:<br> 
+  <input type="radio" id="5" name="gender2" value="5">
+  <label for="5">5</label><br>
+  <input type="radio" id="4" name="gender2" value="4">
+  <label for="4">4</label><br>
+  <input type="radio" id="3" name="gender2" value="3">
+  <label for="3">3</label><br>
+  <input type="radio" id="2" name="gender2" value="2">
+  <label for="2">2</label><br>
+  <input type="radio" id="1" name="gender2" value="1">
+  <label for="1">1</label><br><br>
+  Ocjena predavača:<br> 
+  <input type="radio" id="5" name="gender3" value="5">
+  <label for="5">5</label><br>
+  <input type="radio" id="4" name="gender3" value="4">
+  <label for="4">4</label><br>
+  <input type="radio" id="3" name="gender3" value="3">
+  <label for="3">3</label><br>
+  <input type="radio" id="2" name="gender3" value="2">
+  <label for="2">2</label><br>
+  <input type="radio" id="1" name="gender3" value="1">
+  <label for="1">1</label><br><br>
+  
+  <input type="submit" name="Submit1" value="Submit">
+
+</form>
+ 
+
+
+</body>
+</html>
